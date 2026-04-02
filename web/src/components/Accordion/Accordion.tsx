fc7bba6a535176d26653c825adbf6e43a14534bf
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2026-04-02 17:46:10 +0200

    updated accordion padding + rounded

--- web/src/components/Accordion/Accordion.tsx
+++ web/src/components/Accordion/Accordion.tsx
@@ -9,17 +9,17 @@ export function Accordion({ children, label }: AccordionProps) {
       style={{
         interpolateSize: "allow-keywords",
       }}
-      className={`transition-all duration-100 overflow-hidden flex flex-col gap-2 bg-black/20 border-lavender/20 border rounded-xl ${isOpen ? "h-auto" : "h-11"}`}
+      className={`transition-all duration-100 overflow-hidden flex flex-col gap-2 bg-black/20 border-lavender/20 border rounded-3xl ${isOpen ? "h-auto" : "h-11"}`}
     >
       <button
         onClick={() => {
           setIsOpen((prev) => !prev);
         }}
-        className="text-sm text-left font-medium p-3"
+        className="text-sm text-left font-medium px-4 py-3"
       >
         {label}
       </button>
-      <span className="px-3 pb-3">{children}</span>
+      <span className="px-4 pb-4">{children}</span>
     </div>
   );
 }
