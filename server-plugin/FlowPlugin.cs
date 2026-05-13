8133c1722883c4bdacb15ec1b030fe76a2843bee
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2026-05-13 15:55:05 +0200

    Added some meta

--- server-plugin/FlowPlugin.cs
+++ server-plugin/FlowPlugin.cs
@@ -1,14 +1,23 @@
 ﻿using CounterStrikeSharp.API.Core;
+
 namespace FlowServer;
 
 public class FlowPlugin : BasePlugin
 {
     public override string ModuleName => "Flow Server Plugin";
-
     public override string ModuleVersion => "0.0.1";
+    public override string ModuleAuthor => "Sivert";
+    public override string ModuleDescription => "Basic Flow server plugin";
 
     public override void Load(bool hotReload)
     {
-        Console.WriteLine("Flow Server Plugin loaded!");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+
+        Console.WriteLine("========================================");
+        Console.WriteLine("[FLOW SERVER] Plugin loaded!");
+        Console.WriteLine($"[FLOW SERVER] Hot reload: {hotReload}");
+        Console.WriteLine("========================================");
+
+        Console.ResetColor();
     }
 }
